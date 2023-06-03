6ea5e9e6bad201c128ba1bbca5d163f3b7579d1b
Jose Bryan <jbsg97@gmail.com>
2023-06-03 14:14:38 -0700

    Chore: Se modifican codigos HTTP

--- app/api/api_v1/endpoints/clients.py
+++ app/api/api_v1/endpoints/clients.py
@@ -11,7 +11,7 @@ router = APIRouter()
 def get_clients(db: Session = Depends(deps.get_db)):
     db_client = clients.get_clients(db)
     if not db_client:
-        raise HTTPException(status_code=204, detail="No existen clientes.")
+        raise HTTPException(status_code=400, detail="No existen clientes.")
     return db_client
 
 
@@ -19,23 +19,23 @@ def get_clients(db: Session = Depends(deps.get_db)):
 def get_clients(cellphone: str, db: Session = Depends(deps.get_db)):
     db_client = clients.get_client(db, cellphone)
     if not db_client:
-        raise HTTPException(status_code=204, detail="El cliente no existe.")
+        raise HTTPException(status_code=400, detail="El cliente no existe.")
     return db_client
 
 
 @router.post("", response_model=ClientBase, status_code=201)
 def create_client(client: ClientCreate, db: Session = Depends(deps.get_db)):
-    db_client = clients.create_client(db, client.telefono, client=client)
-    if not db_client:
+    created_client = clients.create_client(db, client.telefono, client=client)
+    if not created_client:
         raise HTTPException(status_code=400, detail="El telefono ya ha sido registrado.")
-    return db_client
+    return created_client
 
 
 @router.put("/{cellphone}", response_model=ClientBase)
 def update_client(cellphone: str, client: ClientUpdate, db: Session = Depends(deps.get_db)):
     updated_client = clients.update_client(db, cellphone, client)
     if not updated_client:
-        raise HTTPException(status_code=204, detail="El cliente no existe.")
+        raise HTTPException(status_code=400, detail="El cliente no existe.")
     return updated_client
 
 
@@ -43,5 +43,5 @@ def update_client(cellphone: str, client: ClientUpdate, db: Session = Depends(de
 def delete_client(cellphone: str, db: Session = Depends(deps.get_db)):
     deleted_client = clients.delete_client(db, cellphone)
     if not deleted_client:
-        raise HTTPException(status_code=204, detail="El cliente no existe.")
+        raise HTTPException(status_code=400, detail="El cliente no existe.")
     return deleted_client
