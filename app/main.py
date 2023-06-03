12c7a94c50a8a06570a2e9bf86137d83ed81d484
Jose Bryan <jbsg97@gmail.com>
2023-06-03 14:13:46 -0700

    Chore: Se agrega variable de entorno

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
+import os
+
 from fastapi import FastAPI
 from app.api.api_v1.api import api_router
 from app.database.database import Base
-from app.database.database import SessionLocal, engine
+from app.database.database import engine
 from mangum import Mangum
+from dotenv import load_dotenv
 
-app = FastAPI()
+load_dotenv()
+
+app = FastAPI(
+    root_path = os.getenv('STAGE')
+)
 Base.metadata.create_all(bind=engine)
-
 
 @app.get("/")
 async def root():
